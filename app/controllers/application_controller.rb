33713a14dda0cfcd1c670554701419ead3c96131
Jared Grove <jaredkgrove@gmail.com>
2019-07-12 00:28:34 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,9 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-    erb :index
-  end
+  # get '/' do
+  #   erb :index
+  # end
 
   get "/articles/new" do
     erb :new
